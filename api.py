c4e334da2ec04d502de8415c7bb73b88d6ea0a8a
Vivek15494 <vivek15494@gmail.com>
2018-08-12 14:43:16 +0530

    Models

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template,redirect,url_for,session,escape
 
+from models.user_model import user_signup, search_user_by_username
+
 app = Flask(__name__)
 app.secret_key = 'any random string'
 
@@ -11,25 +13,35 @@ def index():
 		return render_template('welcome.html', login= "False")
 
 @app.route("/login", methods=['POST'])
-def login():	
-	if(request.form["password"] == "what" and request.form["username"] == "shashank"):
+def login():
+	inbound_username = request.form["username"]
+	existing_user = search_user_by_username(inbound_username)
+	if(existing_user is None):
+		return render_template('error.html', message="You have to sign up first")
+	
+	elif(request.form["password"] == "what" and request.form["username"] == "shashank"):
 		print ("Login successful. Redirecting to products page")
-		session['username'] = request.form ["username"]
+		session['username'] = str(existing_user['_id'])
 		return redirect(url_for('index'))
 
 	else:
-	    	return render_template('error.html')
+	    	return render_template('error.html', message="username or password not correct")
 
 @app.route("/signup", methods=["POST"])
 def signup():
 	user_info={}
-	user_info["firstname"] = request.form["firstname"]
-	user_info["lastname"] = request.form["lastname"]
+	user_info["username"] = request.form["username"]
+	user_info["name"] = request.form["name"]
 	user_info["email"] = request.form["email"]
 	user_info["password"] = request.form["password"]
+	
+	#import pdb;pdb.set_trace()
+	results =user_signup(user_info)
+	if(results is True):
 
-	session['username'] = user_info
-	return redirect(url_for('index'))
+	 session['user_id'] = str(user_info['_id'])
+	return("successfully saved")
+	#return redirect(url_for('index'))
 
 
 @app.route("/products")
@@ -38,7 +50,7 @@ def product_page():
 
 @app.route('/logout')
 def logout():
-	session.pop('username', None)
+	session.pop('user_id', None)
 	return redirect(url_for('index'))
 
 if (__name__ == "__main__"):
---
+++ models/user_model.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+client = MongoClient()
+
+db = client['session']
+
+def user_signup(user_info):
+  #here we have to save the user_info dict inside mongo
+
+  results = db['users'].insert_one(user_info)
+  return True
+
+def search_user_by_username(username):
+  filter_query = {'username' : username}
+  results = db["users"].find(filter_query)
+
+  if(results.count()> 0):
+    return results.next()
+  else:
+    return None
